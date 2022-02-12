60e5a0d639a54ab05e719fbce6600c1862e99389
Victor EJiofor <victorejiofor3349@gmail.com>
2022-02-12 22:53:49 +0100

    Added text and image

--- frontend/src/components/HompageCard.js
+++ frontend/src/components/HompageCard.js
@@ -5,6 +5,9 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Paper } from '@mui/material';
+
+
 
 const bull = (
   <Box
@@ -17,26 +20,26 @@ const bull = (
 
 export default function BasicCard() {
   return (
+    <Paper sx={{ width: '50%'}}>
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        <Typography variant="h5" component="div" color="text.secondary" gutterBottom>
           Word of the Day
         </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        Lorem ipsum dolor sit amet, adhuc homero scribentur his id, ad sint animal vix. 
+        Quas ridens eum eu, no vide legimus usu. Ea natum reque animal qui. His ex virtute appareat 
+        pertinacia. Has facilisis dissentiunt id, id labores invenire iudicabit eum. 
+        Pro id quando partiendo, simul temporibus vix ut, an erant nobis sed. Sale lorem ea cum. 
+        Sed te doctus commune accumsan, euripidis constituam ne pro. Ludus mandamus eum no, soluta 
+        mnesarchum an pro. Ad labore timeam principes vis, eos ad augue delicata signiferumque. 
+        Ea eum luptatum adolescens. Eu eos duis tota volumus, no qui doming virtute 
         </Typography>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
+    </Paper>
   );
 }
