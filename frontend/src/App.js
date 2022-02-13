836a58a885870dbdb8712b7d12d35882e4e214dd
Harshal0902 <harshalraikwar07@gmail.com>
2022-02-13 22:12:37 +0530

    💥 Change user auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,35 +11,49 @@ import Main from './container/Main'
 import Homepage from './Pages/Homepage';
 import Welcome from './Pages/WelcomePage';
 import { auth } from './services/firebase.js'
+// import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './services/firebase.js'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import firebase from 'firebase'
 
 export default function App() {
 
-    const { authenticate, isAuthenticated, logout } = useMoralis();
+    // const { authenticate, isAuthenticated, logout } = useMoralis();
     const [user] = useAuthState(auth);
 
-    if (isAuthenticated) {
-        return (
-            <div>
-                <Button onClick={logout}>Logout</Button>
-                {/* <Main /> */}
-                <Welcome />
-            </div>
-        )
+    function signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
     }
+
+    // if (isAuthenticated) {
+    //     return (
+    //         <div>
+    //             <Button onClick={logout}>Logout</Button>
+    //             {/* <Main /> */}
+    //             <Welcome />
+    //         </div>
+    //     )
+    // }
 
     return (
         <div>
-            <Homepage />
-            <CardActions>
-                <Paper sx={{ display: 'flex', flexDirection: 'column', marginTop: '30%', position: 'relative', mx: "auto" }}>
-                    <Box component="span" sx={{ p: 7, border: '5px grey' }}>
-                        <Button variant="contained" color="success" sx={{ width: "100%" }} href="/Welcome">Login with google</Button>
-                        <Paper sx={{ margin: '5%' }}></Paper>
-                    </Box>
-                    <Button variant="contained" sx={{ width: "100%" }} onClick={() => authenticate()} >Log in with metamask</Button>
-                </Paper>
-            </CardActions>
+            {user ?
+                <Main />
+                :
+                <div>
+                    <Homepage />
+                    <CardActions>
+                        <Paper sx={{ display: 'flex', flexDirection: 'column', marginTop: '30%', position: 'relative', mx: "auto" }}>
+                            <Box component="span" sx={{ p: 7, border: '5px grey' }}>
+                                <Button variant="contained" color="success" sx={{ width: "100%" }} onClick={signInWithGoogle}>Login with google</Button>
+                                <Paper sx={{ margin: '5%' }}></Paper>
+                            </Box>
+                            <Button variant="contained" sx={{ width: "100%" }} onClick={() => authenticate()} >Log in with metamask</Button>
+                        </Paper>
+                    </CardActions>
+                </div>
+            }
         </div>
     )
 }
